ae445e1ae5f9b28c8972c82ad2581f75327336cd
Estella Newton <estellanewton@gmail.com>
2020-10-03 09:06:29 -0700

    Initial Commit

---
+++ Ubi Vermis Liber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ubi Vermis Liber
+//
+//  Created by Estella Newton on 10/3/20.
+//  Copyright © 2020 Estella Newton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ubi Vermis LiberTests/Ubi_Vermis_LiberTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ubi_Vermis_LiberTests.swift
+//  Ubi Vermis LiberTests
+//
+//  Created by Estella Newton on 10/3/20.
+//  Copyright © 2020 Estella Newton. All rights reserved.
+//
+
+import XCTest
+@testable import Ubi_Vermis_Liber
+
+class Ubi_Vermis_LiberTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
